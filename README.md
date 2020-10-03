6e65d3e9df0f3f4d2e07772148fe1d9e6a84e31d
Tim Hammond <timhammond@Tims-MacBook-Pro.local>
2020-10-02 20:11:47 -0600

    Changed the cat color from black to green

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Lakin Moser is Person B
 
 Tim created the project
 
-Lakin Changed the draw inputs in main from 50 to 100
+1.) Lakin Changed the draw inputs in main from 50 to 100
 
-tim changed the 
+1.) tim changed the cats eyes from green to red
+
+2.) Lakin Changed mouth height to 30. Mouth is now open.
+
+2.) Tim changed the cat color from black to green
--- src/Cat.java
+++ src/Cat.java
@@ -14,7 +14,7 @@ public class Cat {
 	private static final int EYE_HEIGHT = 20;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 30;
+	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
@@ -27,9 +27,11 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.black);
+		g2.setColor(Color.green);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
+		
+		//changed the color from green to red 
 		g2.setColor(Color.red);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
